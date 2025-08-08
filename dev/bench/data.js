ec56223899d99af84194bf89890f14c7b5c85213
github-action-benchmark <github@users.noreply.github.com>
2025-08-08 03:43:24 +0000

    add Client memory (customSmallerIsBetter) benchmark result for a662a8d5a7ddea04824dabb0806e2ac3edb4610d

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1754624603473,
+  "lastUpdate": 1754624604667,
   "repoUrl": "https://github.com/grumbach/autonomi",
   "entries": {
     "`safe files` benchmarks": [
@@ -69,6 +69,42 @@ window.BENCHMARK_DATA = {
           {
             "name": "Peak memory w/ `safe` benchmarks",
             "value": 86,
+            "unit": "MB"
+          }
+        ]
+      }
+    ],
+    "Client memory": [
+      {
+        "commit": {
+          "author": {
+            "email": "chriso83@protonmail.com",
+            "name": "Chris O'Neil",
+            "username": "jacderida"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "a662a8d5a7ddea04824dabb0806e2ac3edb4610d",
+          "message": "Merge pull request #3153 from maqi/benchmark_charts_correct_download_throughput_calculation\n\nfix(benchmark): correct benchmark chart download throughput calculation",
+          "timestamp": "2025-08-07T22:00:12+01:00",
+          "tree_id": "647a6966063af8f351dcd5cb1d6f433311cfc105",
+          "url": "https://github.com/grumbach/autonomi/commit/a662a8d5a7ddea04824dabb0806e2ac3edb4610d"
+        },
+        "date": 1754624604378,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Peak memory usage w/ upload",
+            "value": 17,
+            "unit": "MB"
+          },
+          {
+            "name": "Average memory usage w/ upload",
+            "value": 15,
             "unit": "MB"
           }
         ]
