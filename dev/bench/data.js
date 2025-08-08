b81b48fc2fa234bfa29f6050001bec51457e6c29
github-action-benchmark <github@users.noreply.github.com>
2025-08-08 03:43:25 +0000

    add swarm_driver long handlings (customSmallerIsBetter) benchmark result for a662a8d5a7ddea04824dabb0806e2ac3edb4610d

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1754624604667,
+  "lastUpdate": 1754624605947,
   "repoUrl": "https://github.com/grumbach/autonomi",
   "entries": {
     "`safe files` benchmarks": [
@@ -106,6 +106,47 @@ window.BENCHMARK_DATA = {
             "name": "Average memory usage w/ upload",
             "value": 15,
             "unit": "MB"
+          }
+        ]
+      }
+    ],
+    "swarm_driver long handlings": [
+      {
+        "commit": {
+          "author": {
+            "email": "chriso83@protonmail.com",
+            "name": "Chris O'Neil",
+            "username": "jacderida"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "a662a8d5a7ddea04824dabb0806e2ac3edb4610d",
+          "message": "Merge pull request #3153 from maqi/benchmark_charts_correct_download_throughput_calculation\n\nfix(benchmark): correct benchmark chart download throughput calculation",
+          "timestamp": "2025-08-07T22:00:12+01:00",
+          "tree_id": "647a6966063af8f351dcd5cb1d6f433311cfc105",
+          "url": "https://github.com/grumbach/autonomi/commit/a662a8d5a7ddea04824dabb0806e2ac3edb4610d"
+        },
+        "date": 1754624605623,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "swarm_driver long handling times",
+            "value": 11399,
+            "unit": "hits"
+          },
+          {
+            "name": "swarm_driver long handling total_time",
+            "value": 23502,
+            "unit": "ms"
+          },
+          {
+            "name": "swarm_driver average long handling time",
+            "value": 2,
+            "unit": "ms"
           }
         ]
       }
