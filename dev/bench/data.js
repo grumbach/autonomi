d8ab002c4d4aa884ede19dca1a597e0823fdb114
github-action-benchmark <github@users.noreply.github.com>
2025-08-08 03:42:11 +0000

    add `safe files` benchmarks (customBiggerIsBetter) benchmark result for a662a8d5a7ddea04824dabb0806e2ac3edb4610d

---
+++ dev/bench/data.js
@@ -0,0 +1,47 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1754624531111,
+  "repoUrl": "https://github.com/grumbach/autonomi",
+  "entries": {
+    "`safe files` benchmarks": [
+      {
+        "commit": {
+          "author": {
+            "email": "chriso83@protonmail.com",
+            "name": "Chris O'Neil",
+            "username": "jacderida"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "a662a8d5a7ddea04824dabb0806e2ac3edb4610d",
+          "message": "Merge pull request #3153 from maqi/benchmark_charts_correct_download_throughput_calculation\n\nfix(benchmark): correct benchmark chart download throughput calculation",
+          "timestamp": "2025-08-07T22:00:12+01:00",
+          "tree_id": "647a6966063af8f351dcd5cb1d6f433311cfc105",
+          "url": "https://github.com/grumbach/autonomi/commit/a662a8d5a7ddea04824dabb0806e2ac3edb4610d"
+        },
+        "date": 1754624530637,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "ant files upload 1mb",
+            "value": 2.515104006593593,
+            "unit": "MiB/s"
+          },
+          {
+            "name": "ant files upload 10mb",
+            "value": 9.806053319537185,
+            "unit": "MiB/s"
+          },
+          {
+            "name": "ant files download",
+            "value": 27.179534224283596,
+            "unit": "MiB/s"
+          }
+        ]
+      }
+    ]
+  }
+}
