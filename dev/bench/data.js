204a5e118549ea44d09299a5ab5456e55da20d79
github-action-benchmark <github@users.noreply.github.com>
2025-09-25 09:24:24 +0000

    add swarm_driver long handlings (customSmallerIsBetter) benchmark result for 5a54c3a841ef7b58bc9546ee36b0859e0d07138f

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1758792263357,
+  "lastUpdate": 1758792264719,
   "repoUrl": "https://github.com/grumbach/autonomi",
   "entries": {
     "`safe files` benchmarks": [
@@ -2217,6 +2217,45 @@ window.BENCHMARK_DATA = {
           {
             "name": "swarm_driver long handling total_time",
             "value": 21731,
+            "unit": "ms"
+          },
+          {
+            "name": "swarm_driver average long handling time",
+            "value": 2,
+            "unit": "ms"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "qi.ma@maidsafe.net",
+            "name": "maqi",
+            "username": "maqi"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": false,
+          "id": "5a54c3a841ef7b58bc9546ee36b0859e0d07138f",
+          "message": "Merge pull request #3208 from maqi/using_tokio_spawn_blocking\n\nchore(client): use tokio::spawn_blocking",
+          "timestamp": "2025-09-23T21:22:09Z",
+          "tree_id": "72294d1770fa9ef5263d0460c3d3105252c1071b",
+          "url": "https://github.com/grumbach/autonomi/commit/5a54c3a841ef7b58bc9546ee36b0859e0d07138f"
+        },
+        "date": 1758792264387,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "swarm_driver long handling times",
+            "value": 10164,
+            "unit": "hits"
+          },
+          {
+            "name": "swarm_driver long handling total_time",
+            "value": 21432,
             "unit": "ms"
           },
           {
