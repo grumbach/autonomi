0cbcfa1e0aa246e0cf93f02ecaadedf4e803d490
github-action-benchmark <github@users.noreply.github.com>
2026-01-06 16:01:34 +0000

    add Client memory (customSmallerIsBetter) benchmark result for 2822430d8c752a7684fb2700745917d38b7f9563

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1767715293485,
+  "lastUpdate": 1767715294953,
   "repoUrl": "https://github.com/grumbach/autonomi",
   "entries": {
     "`safe files` benchmarks": [
@@ -2611,6 +2611,40 @@ window.BENCHMARK_DATA = {
           "url": "https://github.com/grumbach/autonomi/commit/c15af5a0ed3e0923beea90fe4d77e23f9c10ea54"
         },
         "date": 1765527295432,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Peak memory usage w/ upload",
+            "value": 15,
+            "unit": "MB"
+          },
+          {
+            "name": "Average memory usage w/ upload",
+            "value": 14,
+            "unit": "MB"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "qi.ma@maidsafe.net",
+            "name": "maqi",
+            "username": "maqi"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "2822430d8c752a7684fb2700745917d38b7f9563",
+          "message": "Merge pull request #3391 from maqi/remove_peer_when_failed_to_fetch_version\n\nfeat(node): remove peer when failed to fetch version",
+          "timestamp": "2026-01-05T13:01:01Z",
+          "tree_id": "e1cde438f620b4e84a9ae6d489caf433002d3270",
+          "url": "https://github.com/grumbach/autonomi/commit/2822430d8c752a7684fb2700745917d38b7f9563"
+        },
+        "date": 1767715294482,
         "tool": "customSmallerIsBetter",
         "benches": [
           {
